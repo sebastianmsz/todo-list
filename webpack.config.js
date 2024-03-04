a76c73dcba3f05d8d6239028dc12775a55de19e7
sebastianmsz <sevas.molina2004@gmail.com>
2024-03-04 11:55:05 -0500

    Stablish webpack project structure.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+        clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/template.html',
+			title: 'Todo-List',
+		}),
+	],
+	module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+                        loader: 'file-loader',
+                        options: {
+                          publicPath: 'img',
+                        },
+					},
+				],
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+};
